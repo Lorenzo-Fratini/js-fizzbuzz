72b9accf1f5208aa28b6010f166525cec2622254
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-05 17:27:29 +0100

    first udate

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// console.log('Here');
+
+// metodo 1
+// for (var i=0;i<100;i++) {
+//
+//   var num = i + 1;
+//
+//   if (num % 3 == 0 && num % 5 == 0) {
+//     num = 'FizzBuzz'
+//   } else if (num % 3 == 0) {
+//     num = 'Fizz';
+//   } else if (num % 5 == 0) {
+//     num = 'Buzz';
+//   }
+//
+//   console.log(num);
+// }
