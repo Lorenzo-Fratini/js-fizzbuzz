cb2dc472fc207537f24f2367391bbba679e81232
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-05 18:00:38 +0100

    first upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 // console.log('Here');
 
 // metodo 1
-// for (var i=0;i<100;i++) {
-//
-//   var num = i + 1;
-//
-//   if (num % 3 == 0 && num % 5 == 0) {
-//     num = 'FizzBuzz'
-//   } else if (num % 3 == 0) {
-//     num = 'Fizz';
-//   } else if (num % 5 == 0) {
-//     num = 'Buzz';
-//   }
-//
-//   console.log(num);
-// }
+for (var i=0;i<100;i++) {
+
+  var num = i + 1;
+
+  if (num % 3 == 0 && num % 5 == 0) {
+    num = 'FizzBuzz'
+  }
+
+  if (num % 3 == 0) {
+    num = 'Fizz';
+  } else if (num % 5 == 0) {
+    num = 'Buzz';
+  }
+
+  console.log(num);
+}
+
+// metodo 2
